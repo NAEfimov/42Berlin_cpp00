2df30939548616782cd1cebd2fe457b350534edf
nefimov <nefimov@student.42berlin.de>
2025-09-24 10:49:13 +0200

    add std::getline input

--- ex01/mapb.cpp
+++ ex01/mapb.cpp
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 15:03:12 by nefimov           #+#    #+#             */
-/*   Updated: 2025/09/23 21:07:20 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/09/24 10:43:39 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,33 @@ int		read_index(int max_index);
 
 int main() {
 	PhoneBook			phone_book;
-	std::string			input;
+	std::string			command;
 
 	std::cout << "====================" << std::endl;
 	std::cout << "My Awesome PhoneBook" << std::endl;
 	std::cout << "====================" << std::endl;
 	std::cout << "Enter one of the following commands: ADD, SEARCH or EXIT" << std::endl << std::endl;
 	
-	while (input != EXIT) {
+	while (true) {
 		std::cout << "phonebook> ";
-		std::cin >> input;
-		// add contact to the phonebook
-		if (input == ADD) {
+		if (!std::getline(std::cin, command)) {
+			std::cout << std::endl;
+            break; 
+        }
+		
+		if (command == ADD) {
 			phone_book.add_contact(read_contact());
 			std::cout << std::endl;
-			continue;
-		}
-		// search contact in the phonebook
-		if (input == SEARCH) {
+		} else if (command == SEARCH) {
 			if (phone_book.get_num() == 0) {
 				std::cout << "PhoneBook is empty" << std::endl;
 				continue;
 			}
 			phone_book.display_contacts();
 			phone_book.display_contact(read_index(phone_book.get_num()) - 1);
-			// std::cout << std::endl;
-			continue;
+			
+		} else if (command == "EXIT") {
+			break;
 		}			
 	}
 		
@@ -63,15 +64,15 @@ Contact read_contact(void) {
 	std::string	darkest_secret;
 	
 	std::cout << "First name: ";
-	std::cin >> first_name;
+	std::getline(std::cin, first_name);
 	std::cout << "Last name: ";
-	std::cin >> last_name;
+	std::getline(std::cin, last_name);
 	std::cout << "Nickname: ";
-	std::cin >> nickname;
+	std::getline(std::cin, nickname);
 	std::cout << "Phone number: ";
-	std::cin >> phone_number;
+	std::getline(std::cin, phone_number);
 	std::cout << "Darkest secret: ";
-	std::cin >> darkest_secret;
+	std::getline(std::cin, darkest_secret);
 		
 	return (Contact(first_name, last_name, nickname, phone_number, darkest_secret));
 }
@@ -83,7 +84,8 @@ int read_index(int max_index) {
 	i = 0;
 	while ((input.length() != 1) || (i < 1) || (i > max_index) || (i > MAX_CONTACTS)) {
 		std::cout << "Index to display: ";
-		std::cin >> input;
+		// std::cin >> input;
+		std::getline(std::cin, input);
 		i = input[0] - '0';
 	}
 	return (i);
